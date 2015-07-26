4bca595a927b3a057c295eb4948ff7be5c0dbcfd
zedyang <zed.yang@outlook.com>
2015-07-26 23:33:24 +0800

    1.01

--- container.py
+++ container.py
@@ -1,29 +1,127 @@
+from datetime import datetime, timedelta
 import pandas as pd
+import json
 
 #----------------------------------------------------------------------
-# Financial data structure classes.
-#----------------------------------------------------------------------
-# Tick class
+# Container classes
 
-class Tick(pd.DataFrame):
+class Bar_dy_Equ1d(pd.DataFrame):
 	"""
+	Bar container to collect datayes interday equity data.
+	Loaded time format is datetime(y,m,d,0,0).
+	"""
+	# Initialize as highchart friendly or matplotlib
+	# friendly form.
+	# *It seems like matplotlib.finance added ohlc order.
 
-	"""
+	_time_zone = '+8' # Time zone adjustment for Unix timestamp.
+	_init_as = 'highcharts'
+
+	# column names of imports and outputs.
+	_column_namesf = ['date','openPrice','highestPrice',
+			  		  'lowestPrice','closePrice','turnoverVol']
+	_column_namest = ['time','open','high','low','close','volume']
+	_ohlc_names = ['open','high','low','close']
+	_list_names = ['time','open','high','low','close','volume']
+
+	# column datatypes.
+	_column_dtypes = {'date':'object',
+						'openPrice':'float64',
+			  		 	'highestPrice':'float64',
+			  		 	'lowestPrice':'float64',
+			  		 	'closePrice':'float64',
+			  		 	'turnoverVol':'float64'}
+	# other containers.
+	_buffer = []
+	_cand_List = []
+
 	def __init__(self, data=None, index=None):
-		super(Tick, self).__init__(data=data, index=index,
-			  columns=['time','price','change','volume'])
-		self._dtypes = {'time':'object', 'price':'float64',
-						'change': 'object', 'volume':'float64'}
 
-		self._buffer = []
-		# [{t,price,chg,volume}, ] buffer for real-time appending.
+		if self._init_as == 'highcharts':
+			super(Bar_dy_Equ1d, self).__init__(data=data, index=index,
+			  		 columns=self._column_namesf)
+		else: raise ValueError('_init_as value error.') 
+		# Just for future extensions.
+		self._clean_columns()
 
+	def _clean_columns(self):
+		"""
+		Set names and datetypes of columns.
+		"""
 		for col in self.columns:
-			self[col] = self[col].astype(self._dtypes[col])
-		# Set dtypes.
+			self[col] = self[col].astype(self._column_dtypes[col])
+		self.columns = self._column_namest
 
-#----------------------------------------------------------------------
-# Bar class
+	#----------------------------------------------------------------------
+	# Type conversion methods	
+
+	# Decorator
+	def _to_json(method):
+		"""
+		Decorator, view MongoDB.
+		Require *method returning a Mongo collection.
+		"""
+		def handle_list(*args, **kwargs):
+			try:
+				ret = method(*args, **kwargs) # ret is a list.
+				# convert to json.
+				ret = json.dumps(ret)
+				return ret
+			except Exception,e:
+				raise e
+		return handle_list
+
+	def get_list(self, time='timestamp'):
+		"""
+		Get list format.
+		[[time, open, high, low, close, volume], ]
+
+		Parameters:
+		----------
+		time: string.
+			The format that time is to be presented.
+			* 'timestamp': Unix timestamp, in milliseconds.
+			* 'datetime': Datetime format.
+			* 'index': Integer (starts from zero).
+		"""
+		if not self.empty:
+			df = self
+			if type(time) != str:
+				raise TypeError('[Bar]: Time format must be string.')
+			if time == 'timestamp':
+				to_timestamp = lambda dt: int(dt.strftime("%s"))*1000 + \
+									  	  int(self._time_zone)*3600000
+				df['time'] = df['time'].apply(to_timestamp)
+			elif time == 'datetime':
+				pass
+			elif time == 'index':
+				df['time'] = range(len(df))
+			else:
+				raise ValueError('[Bar]: Unsupported time format.')
+			list_ = df[self._list_names].as_matrix().tolist()
+			return list_
+
+	@_to_json
+	def get_json(self, time='timestamp'):
+		"""
+		Get json format for javascript(highcharts) use.
+		Required format is:
+		[[timestamp.numeric.ms, open, high, 
+		  low, close, volume], [...], ...]
+		
+		Parameters:
+		----------
+		time: string. * same as get_list method.
+		"""
+		return self.get_list(time=time)
+
+	def get_candlist(self):
+		"""
+		Get specified list for matplotlib.finance.candlestick().
+		"""
+		return self.get_candlist(time='index')
+
+
 
 class Bar(pd.DataFrame):
 	"""
@@ -146,46 +244,3 @@ class Bar(pd.DataFrame):
 				t += 1
 		else:
 			pass
-
-#----------------------------------------------------------------------
-# Bar on date class.
-
-class Bar_1d(pd.DataFrame):
-	"""
-
-	"""
-	def __init__(self, data=None, index=None):
-		super(Bar_1d, self).__init__(data=data, index=index,
-			  columns=['date','openPrice','closePrice','highestPrice',
-			  		   'lowestPrice','turnoverVol'])
-		self._dtypes = {'date':'object',
-						'openPrice':'float64',
-						'closePrice':'float64',
-			  		 	'highestPrice':'float64',
-			  		 	'lowestPrice':'float64',
-			  		 	'turnoverVol':'float64'}
-		self._ochl_names = ['open','close','high','low']
-
-		self._buffer = []
-		# [{t,o,c,h,l,v}, ] buffer for real-time appending.
-
-		self._cand_List = []
-		# [(t,o,c,h,l), ] tuples list for matplotlib.finance.ochl_cdstick
-
-		for col in self.columns:
-			self[col] = self[col].astype(self._dtypes[col])
-		# Set dtypes.
-
-		self.columns = ['time','open','close','high','low','volume']
-
-	def get_candlist(self):
-		"""
-		Initialize [[t,o,c,h,l],] list for mpl.finance.candlestick_ochl.
-		"""
-		if not self.empty:
-			_df = self
-			# add index column to temp df.
-			_df['index'] = range(len(_df))
-			return _df[['index','open',
-						'close','high','low']].as_matrix().tolist()
-		else: return None
