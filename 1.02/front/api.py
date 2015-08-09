bc9349aa762538dd72bf0a5e245a383ae19be0e6
zedyang <zed.yang@outlook.com>
2015-08-09 18:29:14 +0800

    reframe

--- 1.02/front/api.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-# Add the ptdraft folder path to the sys.path list
-sys.path.append('/Users/zed/Desktop')
-
-from vnhist2.core import generator, container
-from vnhist2.core.container import Config
--- download.py
+++
@@ -1,6 +0,0 @@
-from base import *
-
-if __name__ == '__main__':
-	
-	ds = DataGenerator()
-	ds.download()
