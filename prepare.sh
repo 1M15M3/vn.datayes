b1e7c5d01886e970ce54f995ccd8aa7dd513c6b4
zedyang <zed.yang@outlook.com>
2015-08-12 16:03:57 +0800

    prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -18,6 +18,31 @@ dc = DBConfig()
 api = PyApi(Config())
 mc = MongodController(dc, api)
 
+mc._collNames['equTicker'] = mc._allEquTickers()
+print '[MONGOD]: Equity tickers collected.'
+
+mc._collNames['secID'] = mc._allSecIds()
+print '[MONGOD]: Security IDs collected.'
+
+mc._collNames['futTicker'] = mc._allFutTickers()
+print '[MONGOD]: Future tickers collected.'
+
+mc._collNames['optTicker'] = mc._allOptTickers()
+print '[MONGOD]: Option symbols collected.'
+
+mc._collNames['fudTicker'] = mc._allFudTickers()
+print '[MONGOD]: Mutual Fund symbols collected.'
+
+mc._collNames['idxTicker'] = mc._allIdxTickers()
+print '[MONGOD]: Index symbols collected.'
+
+mc._ensure_index()
+
+
 EOF
 
 echo [vn-past]: Configuration finished.
+echo [vn-past]: Current collection names: 
+
+cd ./names
+ls -l
